f0dea415f335f773d1b1a6d98f53d91fb1872c23
Saud-Aldossri <44228721+Saud-Aldossri@users.noreply.github.com>
2018-10-17 13:34:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,8 +20,24 @@ client.on('message', message => {
 
 
 
-
-
+client.on('message', message => {
+    if (message.content.startsWith("p!stats")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTitle('Stats Bot / Info ')
+            .addField('``Uptime``', timeCon(process.uptime()), true)
+            .addField('``Ping Is``' , `${Date.now() - message.createdTimestamp}` + '``Ms``', true)
+            .addField('``RAM Usage``', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
+            .addField('``Guild Count``', client.guilds.size, true)
+            .addField('``Bot In channel``' , `${client.channels.size}` , true)
+            .addField('``Users rout``' ,`${client.users.size}` , true)
+            .addField('``Name Bot Or tag``' , `${client.user.tag}` , true)
+            .addField('``Bot Id``' , `${client.user.id}` , true)
+            .setFooter('PowerBOT / Team')
+    })
+}
+});
 
 
 
